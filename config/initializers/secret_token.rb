ddc650ea4ceb1161ad8e220f9868a3ae5cd8500a
masimons <masimons66@gmail.com>
2013-06-30 21:57:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickr::Application.config.secret_token = 'f765f471d41fc74d10fdcf8aec939c9d563fa5c53a4b847f120c7ed83c9240e84245a1b30bb96a37443da439b238857c0d2e3ccfe005bc6a6b42d3446ea83cd6'
